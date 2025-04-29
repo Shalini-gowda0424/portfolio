8eac0a679ac60311c1cdd31ab1fa5c228457cbe5
ShaliniS <121044399+Shalini-gowda0424@users.noreply.github.com>
2025-04-29 11:52:11 +0530

    Update projects.tsx

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,1 +1,11 @@
 // Projects Page
+import React from 'react';
+
+export default function Projects() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold">Projects</h1>
+      <p className="mt-4 text-lg">Here are some cool DevOps projects I've worked on!</p>
+    </div>
+  );
+}
